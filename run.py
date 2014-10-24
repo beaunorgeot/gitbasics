52f308286efa1bc42ea9c49bdfcfa09335ce2152
blakedallen <blakedallen@gmail.com>
2014-10-24 14:31:03 -0700

    adding a recursive factorial function

--- run.py
+++ run.py
@@ -7,18 +7,24 @@ n = int(raw_input("What should n be? > "))
 
 the_answer = []
 
-
-
-
+""" cool function"""
 def factorial(n):
 	x = int(n)
 	while n > 1:
-		print x, n
+		#print x, n
 		x = x * (n-1)
-		
 		n -= 1
 	return x
 
+""" new implementation """
+def recursive_factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
 print factorial(n)
+
+print recursive_factorial(n)
 
 
